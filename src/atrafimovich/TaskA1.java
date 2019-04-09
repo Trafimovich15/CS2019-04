b4c7f38309014c3f504c7b93b377109631db230b
berzhanin <di.berzhanin@gmail.com>
2019-04-09 21:30:51 +0300

    1

---
+++ src/atrafimovich/TaskA1.java
@@ -0,0 +1,19 @@
+package atrafimovich;
+
+public class TaskA1 {
+    public static void main(String[] args) {
+                for (int i=0;i<10;i++) {
+                    if(i==3)
+                    {System.out.println("Меня интересуте высокая зарплата");}
+                    else if (i==9)
+                    {
+                        System.out.println("Меня интересует высокая зарплата");}
+
+                        System.out.println("Я изучаю программирование");}
+
+            }
+
+
+                    }
+
+
